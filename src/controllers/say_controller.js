23234c933f4bb687f46db932ab63cd85ded2c28d
jaydeaton <jayrdeaton@gmail.com>
2019-05-04 15:37:57 -0500

    added voice and speed

--- src/controllers/say_controller.js
+++ src/controllers/say_controller.js
@@ -1,13 +1,14 @@
 const express = require('express'),
   router = express.Router(),
-  { runCommand } = require('../helpers');
+  say = require('say');
 
 router.route('/')
   .post(async (req, res, next) => {
-    const { message } = req.body;
+    const { message, voice, speed } = req.body;
     if (!message) return res.status(200).send();
     try {
-      await runCommand(`say ${message}`);
+      say.stop();
+      say.speak(message, voice, speed);
       res.status(200).send();
     } catch(err) {
       res.status(500).send(err);

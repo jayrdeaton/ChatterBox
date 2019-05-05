5a3d2f59dccf408d467c9ab2e5b4676cb0dadae8
jaydeaton <jayrdeaton@gmail.com>
2019-05-04 20:28:13 -0500

    first fetch!

--- client/src/containers/SayForm.js
+++ client/src/containers/SayForm.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { Button, TextField } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
+const DOMAIN = process.env.REACT_APP_SAYPI_DOMAIN || '';
+
 class SayForm extends Component {
+  state = { message: '' };
+  handleMessageChange = (e) => {
+    this.setState({ message: e.target.value });
+  };
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { message } = this.state;
+    try {
+      await fetch(`${DOMAIN}/api/say`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'post',
+        body: JSON.stringify({ message })
+      });
+    } catch(err) {
+      console.log(err);
+    };
+  };
   render() {
     const { classes } = this.props;
+    const { message } = this.state;
     return (
-      <div>SayForm</div>
+      <form onSubmit={this.handleSubmit}>
+        <TextField
+          id='message'
+          label='Message'
+          className={classes.textField}
+          value={message}
+          onChange={this.handleMessageChange}
+          variant='outlined'
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          type='submit'
+        >
+          Say It
+        </Button>
+      </form>
     );
   };
 };

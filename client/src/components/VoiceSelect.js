58d0a3e2ad55deb3dc1c16a5bd51ee850d2ed3b7
jaydeaton <jayrdeaton@gmail.com>
2019-05-04 20:34:59 -0500

    added voice select

---
+++ client/src/components/VoiceSelect.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from '@material-ui/core';
+import { voices } from '../refs';
+
+class VoiceSelect extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor='voice-select'>Voice</InputLabel>
+        <Select
+          value={this.props.selection ? this.props.selection : ''}
+          onChange={this.props.handleChange}
+          inputProps={{
+            name: 'voice',
+            id: 'voice-select'
+          }}
+        >
+          {voices.english.map((index, voice) => 
+            <MenuItem key='index' value={voice}>{Voice}</MenuItem>
+          )}
+        </Select>
+      </FormControl>
+    );
+  };
+};
+const styles = theme => ({
+  formControl: {
+    minWidth: 120,
+  }
+});
+VoiceSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  selection: PropTypes.string
+};
+VoiceSelect = withStyles(styles)(VoiceSelect);
+export default VoiceSelect;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import ErrorDialog from './ErrorDialog';
 import SearchInput from './SearchInput';
 import SortSelect from './SortSelect';
+import VoiceSelect from './VoiceSelect';
 
 export {
   ErrorDialog,
   SearchInput,
-  SortSelect
+  SortSelect,
+  VoiceSelect
 };
